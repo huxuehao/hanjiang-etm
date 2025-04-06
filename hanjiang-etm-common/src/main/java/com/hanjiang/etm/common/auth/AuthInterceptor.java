b92a2bde7e14847d337dd74aa6e210db3a19d6f4
huxuehao <studioustiger@163.com>
2025-04-06 18:44:03 +0800

    处理访问token过期后自动刷新失败的问题

--- hanjiang-etm-common/src/main/java/com/hanjiang/etm/common/auth/AuthInterceptor.java
+++ hanjiang-etm-common/src/main/java/com/hanjiang/etm/common/auth/AuthInterceptor.java
@@ -51,6 +51,7 @@ public class AuthInterceptor implements HandlerInterceptor {
             /* 验证Token，token错误或过期则会报错 */
             Claims claims = TokenUtil.parseToken(token);
         } catch (Exception e) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             throw new NotAuthException();
         }
 
